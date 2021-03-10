92ceb6fb49da674b7253e1444bfd8c8a9b773bb4
Verdant31 <joao.pedro3110@hotmail.com>
2021-03-10 15:30:29 -0400

    first commit

---
+++ android/app/src/main/kotlin/com/example/Despesas/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Despesas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/transaction.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart';
+
+class Transaction {
+  final String id;
+  final String title;
+  final double value;
+  final DateTime date;
+
+  Transaction({
+    @required this.date,
+    @required this.title,
+    @required this.id,
+    @required this.value,
+  });
+}
